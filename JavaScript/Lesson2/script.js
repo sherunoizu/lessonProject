2eef7d922fde16e06a969e4ed70cfe6f31a9d4f2
Nikita <example@gmail.com>
2020-11-16 01:37:58 +0300

    Lesson complite

--- JavaScript/Lesson2/script.js
+++ JavaScript/Lesson2/script.js
@@ -2,8 +2,6 @@
 
 const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?','');
 
-console.log(numberOfFilms);
-
 const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
@@ -11,3 +9,13 @@ const personalMovieDB = {
     genres: [],
     privat: false
 };
+
+const a = prompt('Один из последних просмотренных фильмов?',''),
+      b = prompt('На сколько его оцените?'),
+      c = prompt('Один из последних просмотренных фильмов?',''),
+      d = prompt('На сколько его оцените?');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
