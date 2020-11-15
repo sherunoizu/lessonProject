f15a20093dda1c7390f47ae153e38f3531fca50f
Nikita <example@gmail.com>
2020-11-16 01:29:40 +0300

    Second stage complite

--- JavaScript/Lesson2/script.js
+++ JavaScript/Lesson2/script.js
@@ -1,5 +1,13 @@
 'use strict';
 
-const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?','');
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?','');
 
 console.log(numberOfFilms);
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
