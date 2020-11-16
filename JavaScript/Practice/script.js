f62fb8e806c17f48e8a6e368d9d4fbfa2e835631
Nikita <example@gmail.com>
2020-11-16 03:42:50 +0300

    Practice_2

--- JavaScript/Practice/script.js
+++ JavaScript/Practice/script.js
@@ -10,12 +10,29 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?',''),
-      b = prompt('На сколько его оцените?'),
-      c = prompt('Один из последних просмотренных фильмов?',''),
-      d = prompt('На сколько его оцените?');
+for (let i = 0; i < 2; ++i) {
+    const a = prompt('Один из последних просмотренных фильмов?',''),   
+          b = prompt('На сколько его оцените?');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+    if (a != null && b != null && a != '' && b != '' && a.length < 50 && b.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        i--;
+        console.log('errorr');
+    }
+}
+
+if (personalMovieDB.count < 10) {
+    console.log('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count > 10 && personalMovieDB.count < 30) {
+    console.log('Вы классический зритель')
+} else if (personalMovieDB.count >= 30) {
+    console.log('Вы киноман!');
+} else {
+    console.log('Ошибка.');
+}
+
+
 
 console.log(personalMovieDB);
