aeacc434f7d4cb95bfc874a86c2152cbb41cf672
Nikita <example@gmail.com>
2020-11-22 17:33:37 +0300

    Lesson 16 complite

--- JavaScript/Lesson1/js/script.js
+++ JavaScript/Lesson1/js/script.js
@@ -1,5 +1,22 @@
 'use strict';
 
-for (let i = 1; i < 8; ++i) {
-    console.log(i++);
+function showFirstMessage(text) {
+    console.log(text);
+    let num = 20; 
 }
+
+showFirstMessage("Hello World"); 
+
+// function calc(a,b) {
+//     return (a+b);
+// }
+
+// console.log(calc(4,5 ));
+
+const logger = function () {
+    console.log("hello");
+};
+
+logger();
+
+const calc = (a,b) => a + b;
