ed1a8b207367aa059ce8cbef8911d0cb10389a2d
Nikita <example@gmail.com>
2020-11-25 13:37:12 +0300

    Lesson 22 complite

--- JavaScript/Lesson1/js/script.js
+++ JavaScript/Lesson1/js/script.js
@@ -1,34 +1,96 @@
 "use strict";
 
-const arr = [11,2,13,10,8];
-arr.sort(compareNum);
-console.log(arr);
+const video = ['youtube', 'vimeo', 'rutube'],
+      blogs = ['wordpress', 'livejournal', 'blogger'],
+      internet = [...video, ...blogs, 'vk', 'facebook'];
 
-function compareNum(a,b) {
-    return a-b;
+console.log(internet);
+
+function log(a,b,c) {
+    console.log(a);
+    console.log(b);
+    console.log(c);
 }
-// // arr[99] = 0;
-// // // console.log(arr.length);
 
-// // arr.forEach(function(item,i,arr) {
-// //     console.log(`${i}: ${item}, внутри массива ${arr}`);
-// // });
+const num = [2, 5, 7];
 
+log(...num);
 
-// // //Конец массива
-// // arr.pop();
-// // arr.push(10);
+const array = ['a', 'b'];
 
-// // for (let i = 0; i < arr.length; i++) {
-// //     console.log(arr[i]);
-// // }
+const newArray = [...array];
 
-// for (let value of arr) {
-//     console.log(value);
+const q = {
+    one: 1,
+    two: 2
+};
+
+const newObj = {...q};
+
+// function copy(mainObj) {
+//     let objCopy = {};
+
+//     let key;
+//     for (key in mainObj) {
+//         objCopy[key] = mainObj[key];
+//     }
+//     return objCopy;
 // }
 
-// const str = prompt("", "");
-// const products = str.split(', ');
-// products.sort();
-// console.log(products.join('; '));
+// const numbers = {
+//     a: 2,
+//     b: 5,
+//     c: {
+//         x: 7,
+//         y: 4
+//     }
+// };
+
+// const newNumbers = copy(numbers);
+
+// newNumbers.a = 10;
+// newNumbers.c.x = 10; 
+
+// // console.log(newNumbers);
+// // console.log(numbers);
+
+// const add = {
+//     d: 17,
+//     e: 20
+// };
+
+// const clone = Object.assign({}, add);
+
+// clone.d =20;
+
+// console.log(clone);
+// console.log(add);
+
+// const oldArray = ['a', 'b', 'c'];
+// const newArray = oldArray.slice();
+
+// newArray[1] = 'asdfa';
+// console.log(newArray);
+// console.log(oldArray);
+
+
+// let a = 5,
+//     b = a;
+
+// b = b + 5;
+// console.log(b);
+// console.log(a);
+
+// const obj = {
+//     a: 5,
+//     b: 1
+// };
+
+// // const copy = obj;
+
+// // copy.a = 10;
+
+// // console.log(copy);
+// // console.log(obj);
+
 
