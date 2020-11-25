682926d8be4d5e6680a9223fdd74273f73d33cf6
Nikita <example@gmail.com>
2020-11-25 12:48:42 +0300

    Lesson 20 complite

--- JavaScript/Lesson1/js/script.js
+++ JavaScript/Lesson1/js/script.js
@@ -1,45 +1,34 @@
 "use strict";
 
-const options = {
-    name: 'test',
-    width: 1024,
-    height: 1024,
-    colors: {
-        border: 'black',
-        bg: 'red'
-    },
-    makeTest: function() {
-        console.log(`Test`);
-    }
-};
+const arr = [11,2,13,10,8];
+arr.sort(compareNum);
+console.log(arr);
 
-options.makeTest();
+function compareNum(a,b) {
+    return a-b;
+}
+// // arr[99] = 0;
+// // // console.log(arr.length);
 
-const {border,bg} = options.colors;
-console.log(border);
+// // arr.forEach(function(item,i,arr) {
+// //     console.log(`${i}: ${item}, внутри массива ${arr}`);
+// // });
 
 
+// // //Конец массива
+// // arr.pop();
+// // arr.push(10);
 
+// // for (let i = 0; i < arr.length; i++) {
+// //     console.log(arr[i]);
+// // }
 
-// console.log(Object.keys(options).length);
-
-
-
-
-// console.log(options.name);
-
-// delete (options.name);
-
-// console.log(options);
-
-// for (let key in options) {
-//     if (typeof(options[key]) === 'object') {
-//         for (let i in  options[key]) {
-//             console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
-//         }
-//     } else {
-//         console.log(`Свойство ${key} имеет значение ${options[key]}`);
-//     }
-    
+// for (let value of arr) {
+//     console.log(value);
 // }
+
+// const str = prompt("", "");
+// const products = str.split(', ');
+// products.sort();
+// console.log(products.join('; '));
 
