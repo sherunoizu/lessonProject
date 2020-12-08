006b683a4486c124be391317b5ac5f1b72005b91
Nikita <example@gmail.com>
2020-12-08 19:51:47 +0300

    Prank

--- JavaScript/Practice/script.js
+++ JavaScript/Practice/script.js
@@ -24,7 +24,7 @@ const personalMovieDB = {
         if (personalMovieDB.count < 10) {
             console.log('Просмотрено довольно мало фильмов');
         } else if (personalMovieDB.count > 10 && personalMovieDB.count < 30) {
-            console.log('Вы классический зритель')
+            console.log('Вы классический зритель');
         } else if (personalMovieDB.count >= 30) {
             console.log('Вы киноман!');
         } else {
